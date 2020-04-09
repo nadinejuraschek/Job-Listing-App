dad772159b42334d785da5ab9ea487a0fe30f17b
Nadine Ju <nadine.jura@online.de>
2020-04-09 13:22:42 -0700

    API data fetched

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // REACT
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // COMPONENTS
 import JobBoard from './components/JobBoard';
@@ -8,9 +8,29 @@ import JobBoard from './components/JobBoard';
 import data from './assets/data.json';
 
 function App() {
+  const [ jobs, setJobs ]= useState([]);
+
+  useEffect(() => {
+    setJobs(data);
+    // if there was an API
+    // fetch('API_URL',)
+    // .then((res) => res.json())
+    // .then((data)=> {
+    //   setJobs(data)
+    // });
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div>
-      <JobBoard />
+      {
+        jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+          jobs.map((job) => <JobBoard key={job.id} job={job} /> )
+        )
+      }
     </div>
   );
 }
