adc29df4b4836c5808e1c02a0f5831876ad8733d
Nadine Ju <nadine.jura@online.de>
2020-04-09 19:37:26 -0700

    new and featured tags added

--- src/components/JobBoard.js
+++ src/components/JobBoard.js
@@ -1,23 +1,29 @@
 // REACT
 import React from 'react';
 
-const JobBoard = ({ job: {logo, company, position, postedAt, contract, location, languages, tools} }) => {
-    const languagesTools = [];
+const JobBoard = ({ job: {logo, company, position, postedAt, contract, location, languages, tools, isNew, featured, role, level} }) => {
+    const tags = [role, level];
 
     if (languages) {
-        languagesTools.push(...languages);
+        tags.push(...languages);
     }
     if (tools) {
-        languagesTools.push(...tools);
+        tags.push(...tools);
     }
 
     return (
-        <div className="flex bg-white shadow-md m-4 p-4">
+        <div className={`flex flex-col bg-white shadow-md m-4 p-6 rounded ${
+            featured && "border-l-4 border-teal-500 border-solid"
+        }`}>
             <div>
-                <img src={logo} alt={company} />
+                <img className="w-20 h-20" src={logo} alt={company} />
             </div>
             <div className="flex flex-col justify-between ml-4">
-                <h3 className="font-bold text-base text-teal-500">{company}</h3>
+                <h3 className="font-bold text-base text-teal-500">
+                    {company}
+                    {isNew && (<span className="text-teal-100 bg-teal-500 font-bold m-2 py-1 px-2 rounded-full">New!</span>)}
+                    {featured && (<span className="text-white bg-gray-800 font-bold m-2 py-1 px-2 rounded-full">Featured</span>)}
+                </h3>
                 <h2 className="font-bold text-xl">{position}</h2>
                 <p className="text-gray-700">
                     {postedAt} &#183; {contract} &#183; {location}
@@ -25,8 +31,8 @@ const JobBoard = ({ job: {logo, company, position, postedAt, contract, location,
             </div>
             <div className="flex items-center ml-auto">
                 {
-                    languagesTools 
-                    ? languagesTools.map((item, index) => <span key={index} className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">{item}</span>) 
+                    tags 
+                    ? tags.map((tag, index) => <span key={index} className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">{tag}</span>) 
                     : ''
                 }
             </div>
